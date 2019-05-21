cca2f7372627edbfa84a359969f1e1d8469f1553
Rich Davis <rdavis@galileosuite.com>
2019-05-21 02:27:53 +0000

    working to_json

--- lib/nmon_commands/core.rb
+++ lib/nmon_commands/core.rb
@@ -5,21 +5,6 @@ require 'zlib'
 
 module NmonCommands
 
-  def self.get(filenames,search='EXTERNAL-(aix|linux)-process,T')
-    ret = {}
-    return ret if filenames.nil?
-    filenames = [ filenames ] if filenames.is_a?(String)
-    filenames.map do |f|
-      file = File.new(f.filename)
-      gz = Zlib::GzipReader.new(file)
-      data = gz.read.lines.select do |o|
-        o.match(Regexp.new(search))
-      end
-      ret[f] = data.flatten.map{ |o| o.split('\n') }
-    end
-    return ret
-  end
-
   def self.get_file_list(customer, uuid)
     loc = "/share/prd01/process/#{customer}/archive/by_uuid/#{uuid}/*.{linux,aix}.gz"
     files = Dir.glob(loc).sort.map{ |f| GpeFile.new(f) }
@@ -27,8 +12,16 @@ module NmonCommands
   end
 
   def self.filter_by_dates(files,start_ts,end_ts)
-    sts = DateTime.parse(start_ts).strftime('%s')
-    ets = DateTime.parse(end_ts).strftime('%s')
+    if start_ts.nil?
+      sts = (DateTime.now - 1).strftime('%s')
+    else
+      sts = DateTime.parse(start_ts).strftime('%s')
+    end
+    if end_ts.nil?
+      ets = DateTime.now.strftime('%s')
+    else
+      ets = DateTime.parse(end_ts).strftime('%s')
+    end
     ret = files.select { |file| file.between?(sts,ets) }
     return ret
   end
--- lib/nmon_commands/gpefile.rb
+++ lib/nmon_commands/gpefile.rb
@@ -1,20 +1,49 @@
+require 'json'
+
 class GpeFile
 
-  attr_reader :filename, :date
+  attr_reader :filename, :date, :json
 
   include Comparable
 
   def initialize(filename)
     @filename = filename
     @date     = make_date(filename)
+    @data     = nil
+    @json     = nil
   end
 
   def to_s
     return @filename
   end
 
+  def data
+    @data = get_data if @data.nil?
+  end
+
+  def to_json
+    ret = {}
+    data.each_with_index do |sample,i|
+      lines = sample.lines('\\n')
+      t, samp, head = lines.shift.split(",",3)
+      ret[:header] = head.chomp('\\n')
+      ret[samp] = lines.map{ |o| o.chomp!('\\n') }
+    end
+    return  ret
+    #return  ret.to_json
+  end
+
   def <=>(cdate)
     @date <=> cdate
+  end
+
+  private
+
+  def get_data
+    search='EXTERNAL-(aix|linux)-process,T'
+    file = File.new(self.filename)
+    gz = Zlib::GzipReader.new(file)
+    return gz.read.lines.select{ |o| o.match(Regexp.new(search)) }
   end
 
   def make_date(filename)
@@ -22,4 +51,7 @@ class GpeFile
     ts = File.basename(filename).match(matcher).captures.join(' ')
     DateTime.strptime(ts,'%Y%m%d %H%M%S %Z').strftime('%s')
   end
+
+
+
 end
