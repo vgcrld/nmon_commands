b7568208fe42a08a762f1de697ac765368bae5f1
Rich Davis <rdavis@galileosuite.com>
2019-08-30 11:20:50 +0000

    start moduler

--- lib/nmon_commands/viewer.rb
+++ lib/nmon_commands/viewer.rb
@@ -7,32 +7,25 @@ module NmonCommands
 
   class Viewer < Sinatra::Base
 
+    def self.start(config)
+      # Configure Sinatra
+      set :port, config.port
+      set :bind, config.bind
+      set :run, config.run
+      set :views, config.views
+      set :public_folder, config.public
+      run!
+    end
+
+    # View a file
     get '/view/:customer/:uuid' do
-      customer = params[:customer]
-      uuid = params[:uuid]
-      files = NmonCommands.get_file_list(customer,uuid)
-      files.first.to_json
+      haml :view
     end
 
-    get '/customer' do
-      Dir.glob('/Users/rdavis/process/*').map{ |o| File.basename(o) }.to_json
-    end
-
-    get '/customer/:customer' do
-      customer = params[:customer]
-      uuid = params[:uuid]
-      Dir.glob("/Users/rdavis/process/#{customer}/archive/by_uuid/*").map do |dir|
-        uuid = File.basename(dir)
-        file = Dir.glob("/Users/rdavis/process/#{customer}/archive/by_uuid/#{uuid}/*").first
-        samp = File.basename(file).split(".").first
-        [ samp, uuid ]
-      end.to_json
-    end
-
-    get '/' do
+    # Information
+    get '/doc' do
       haml :doc
     end
-
 
   end
 
