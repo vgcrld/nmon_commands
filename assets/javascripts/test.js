70ccb51a49e2c8262d3523a14d81745ca754b953
Rich Davis <rdavis@galileosuite.com>
2019-08-30 12:33:00 +0000

    sizes

--- assets/javascripts/test.js
+++ assets/javascripts/test.js
@@ -1,13 +1,13 @@
 
 grid_data = []
 
-start_picker = { view:"datepicker", timepicker:true, label:"Start Date", name:"start", stringResult:true, format:"%d %M %Y at %H:%i", width: 500 }
-end_picker   = { view:"datepicker", timepicker:true, label:"End Date",   name:"end",   stringResult:true, format:"%d %M %Y at %H:%i", width: 500 }
+start_picker = { view:"datepicker", timepicker:true, label:"Start Date", name:"start", stringResult:true, format:"%d %M %Y at %H:%i", width: 300 }
+end_picker   = { view:"datepicker", timepicker:true, label:"End Date",   name:"end",   stringResult:true, format:"%d %M %Y at %H:%i", width: 300 }
 range = {
   view:"daterangepicker", name:"default", label:"Default",
   value:{ start: new Date(), end: webix.Date.add(new Date(), 1, "month") }
 }
-submit = { view: "button", label: "Submit", width: 200 }
+submit = { view: "button", label: "Submit", width: 100 }
 
 webix.ui({
   container: "app",
