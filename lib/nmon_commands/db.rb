1e9d0375aef70e3bef167afd1c21b79bfc439a40
Rich Davis <rdavis@galileosuite.com>
2019-10-11 12:34:43 +0000

    update some doc

--- lib/nmon_commands/db.rb
+++ lib/nmon_commands/db.rb
@@ -9,7 +9,12 @@ module NmonCommands; module DB
 
   DB = YAML.load(File.new(File.join(VAR,"gpe_process.yaml"),'r'))
 
-  def self.get_files_for_customer_with_search(customer,search,filter,limit='*.{linux,aix}.gz')
+  # @param [String] customer - exact name of custmer
+  # @param [Symbol] type - set search column: :name, :uuid, or :type (default :type)
+  # @param [Regexp] filter- filter search for  (default is /linux|aix/)
+  # @param [String] limit - glob filter spec (default is "*.{linux,aix}")
+  # Return [Hash] uuid => [Array] [GpeFile]
+  def self.get_files_for_customer_with_search(customer,search=:type,filter=/aix|linux/,limit='*.gz')
     ret = {}
     path = DB[customer]["path"]
     data = get(customer,search,filter)
@@ -20,8 +25,11 @@ module NmonCommands; module DB
     return ret
   end
 
-  # Return all types for the customer based on searching search with filter
-  def self.get(customer,search,filter)
+  # @param [String] customer - exact name of custmer
+  # @param [Symbol] type - set search column: :name, :uuid, or :type (default :type)
+  # @param [Regexp] limit - filter search for  (default is /linux|aix/)
+  # Return [Array] len of 4 each being an array of customer,name,type,uuid
+  def self.get(customer,search=:type,filter=/aix|linux/)
     data = DB[customer]['details'][search]
     idx = data.map.each_with_index{ |o,i| i if o.match(filter)}.compact
     len = idx.length
@@ -34,19 +42,22 @@ module NmonCommands; module DB
     ]
   end
 
-  # Return all the customers
+  # Return all the customer names
   def self.all_customers
     return DB.keys
   end
 
-  # Return all the uuids for a customer
-  def self.get_detail_hash_customer(customer, type: :type, limit: /linux|aix/)
+  # @param [String] customer - exact name of custmer
+  # @param [Symbol] type - set search column: :name, :uuid, or :type
+  # @param [Regexp] limit - search for  (default is /linux|aix/)
+  # Return an [Array] of [Hashes] with customer, name, type, uuid keys [String]
+  def self.get_detail_hash_for_customer(customer, type: :type, limit: /linux|aix/)
     table = self.get(customer,type,limit)
     csv = self.to_csv(table)
     return csv.map{ |o| o }
   end
 
-  # Return all the types found in the DB
+  # Return an [Array] of all the types found in the DB
   def self.all_types
     return DB.values.map{ |o| o['details'][:type] }.flatten.uniq.sort
   end
